8f9d7e1e27f9d989c48ab8f0584661adb52edde5
Damien Berseron <damien.berseron@alan.eu>
2024-11-26 15:06:52 +0100

    create the repro

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,148 +1,30 @@
-import {
-  Anchor,
-  Button,
-  H1,
-  Paragraph,
-  Separator,
-  Sheet,
-  useToastController,
-  SwitchThemeButton,
-  SwitchRouterButton,
-  XStack,
-  YStack,
-} from '@my/ui'
-import { ChevronDown, ChevronUp, X } from '@tamagui/lucide-icons'
-import { useState } from 'react'
-import { Platform } from 'react-native'
-import { useLink } from 'solito/navigation'
+import { Paragraph, YStack, Button } from '@my/ui'
+import { Popover } from '@tamagui/popover'
 
 export function HomeScreen({ pagesMode = false }: { pagesMode?: boolean }) {
-  const linkTarget = pagesMode ? '/pages-example-user' : '/user'
-  const linkProps = useLink({
-    href: `${linkTarget}/nate`,
-  })
-
   return (
-    <YStack
-      f={1}
-      jc="center"
-      ai="center"
-      gap="$8"
-      p="$4"
-      bg="$background"
-    >
-      <XStack
-        pos="absolute"
-        w="100%"
-        t="$6"
-        gap="$6"
-        jc="center"
-        fw="wrap"
-        $sm={{ pos: 'relative', t: 0 }}
-      >
-        {Platform.OS === 'web' && (
-          <>
-            <SwitchRouterButton pagesMode={pagesMode} />
-            <SwitchThemeButton />
-          </>
-        )}
-      </XStack>
-
-      <YStack gap="$4">
-        <H1
-          ta="center"
-          col="$color12"
-        >
-          Welcome to Tamagui.
-        </H1>
-        <Paragraph
-          col="$color10"
-          ta="center"
-        >
-          Here's a basic starter to show navigating from one screen to another.
-        </Paragraph>
-        <Separator />
-        <Paragraph ta="center">
-          This screen uses the same code on Next.js and React Native.
-        </Paragraph>
-        <Separator />
-      </YStack>
-
-      <Button {...linkProps}>Link to user</Button>
-
-      <SheetDemo />
+    <YStack borderColor="red" borderWidth={2} padding={10}>
+      <Paragraph>should show text:</Paragraph>
+      <Popover defaultOpen>
+        <Popover.Trigger asChild>
+          <Button>Trigger</Button>
+        </Popover.Trigger>
+        <Popover.Content>
+          <YStack>
+            <Paragraph>I should have yellow border on web blue border on native</Paragraph>
+          </YStack>
+        </Popover.Content>
+        <Popover.Adapt platform="native">
+          <YStack borderColor="blue" borderWidth={2} padding={10}>
+            <Popover.Adapt.Contents />
+          </YStack>
+        </Popover.Adapt>
+        <Popover.Adapt platform="web">
+          <YStack borderColor="yellow" borderWidth={2} padding={10}>
+            <Popover.Adapt.Contents />
+          </YStack>
+        </Popover.Adapt>
+      </Popover>
     </YStack>
-  )
-}
-
-function SheetDemo() {
-  const toast = useToastController()
-
-  const [open, setOpen] = useState(false)
-  const [position, setPosition] = useState(0)
-
-  return (
-    <>
-      <Button
-        size="$6"
-        icon={open ? ChevronDown : ChevronUp}
-        circular
-        onPress={() => setOpen((x) => !x)}
-      />
-      <Sheet
-        modal
-        animation="medium"
-        open={open}
-        onOpenChange={setOpen}
-        snapPoints={[80]}
-        position={position}
-        onPositionChange={setPosition}
-        dismissOnSnapToBottom
-      >
-        <Sheet.Overlay
-          animation="lazy"
-          enterStyle={{ opacity: 0 }}
-          exitStyle={{ opacity: 0 }}
-        />
-        <Sheet.Handle bg="$gray8" />
-        <Sheet.Frame
-          ai="center"
-          jc="center"
-          gap="$10"
-          bg="$color2"
-        >
-          <XStack gap="$2">
-            <Paragraph ta="center">Made by</Paragraph>
-            <Anchor
-              col="$blue10"
-              href="https://twitter.com/natebirdman"
-              target="_blank"
-            >
-              @natebirdman,
-            </Anchor>
-            <Anchor
-              color="$purple10"
-              href="https://github.com/tamagui/tamagui"
-              target="_blank"
-              rel="noreferrer"
-            >
-              give it a ⭐️
-            </Anchor>
-          </XStack>
-
-          <Button
-            size="$6"
-            circular
-            icon={ChevronDown}
-            onPress={() => {
-              setOpen(false)
-              toast.show('Sheet closed!', {
-                message: 'Just showing how toast works...',
-              })
-            }}
-          />
-        </Sheet.Frame>
-      </Sheet>
-    </>
   )
 }
